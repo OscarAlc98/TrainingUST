97aaab057db353bc2f1eea35510df359280b46c8
OscarAlc98 <99446529+OscarAlc98@users.noreply.github.com>
2025-05-26 21:10:21 +0000

    Update NewProject.jsx

--- Day-7/src/newProject/NewProject.jsx
+++ Day-7/src/newProject/NewProject.jsx
@@ -2,7 +2,6 @@ import ProjectForm from '../projects/ProjectForm';
 import { Project } from '../projects/Project';
 
 export default function NewProjectPage() {
-  // Proyecto vacío inicial
   const emptyProject = new Project({
     id: 0,
     name: '',
@@ -14,7 +13,7 @@ export default function NewProjectPage() {
   return (
     <>
       <h1>New Project</h1>
-      <ProjectForm project={emptyProject} onCancel={() => {}} /> {/* Pasa un onCancel vacío */}
+      <ProjectForm project={emptyProject} onCancel={() => {}} /> {}
     </>
   );
 }
